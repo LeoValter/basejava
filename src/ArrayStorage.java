a1993666c8fa76c655e10aeb4f5f84cb026713b8
leovalter <leovalter83@gmail.com>
2020-08-19 17:03:47 +0300

    HW01 - fix

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,57 +5,53 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
 
-  Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-  void clear() {
-    Arrays.fill(storage, 0, size(), null);
-  }
-
-  void save(Resume r) {
-    if (r == null) {
-      throw new NullPointerException();
+    void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    if (size() > storage.length) {
-      throw new IndexOutOfBoundsException();
+    void save(Resume r) {
+        if (r.uuid == null) {
+            System.out.println("Failed to save resume. Null cannot be stored...");
+        } else if (size > storage.length - 1) {
+            System.out.println("Failed to save resume. Storage is full...");
+        } else if (get(r.uuid) == null) {
+            storage[size] = r;
+            size++;
+        }
     }
 
-    storage[size()] = r;
-  }
-
-  Resume get(String uuid) {
-    int size = size();
-    for (int i = 0; i < size; i++) {
-      if (uuid.equals(storage[i].uuid)) {
-        return storage[i];
-      }
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
     }
-    return null;
-  }
 
-  void delete(String uuid) {
-    int size = size();
-    for (int i = 0; i < size; i++) {
-      if (uuid.equals(storage[i].uuid)) {
-        System.arraycopy(storage, i + 1, storage, i, size - i - 1);
-        storage[size - 1] = null;
-        return;
-      }
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                storage[size - 1] = null;
+                size--;
+                break;
+            }
+        }
     }
-  }
 
-  /**
-   * @return array, contains only Resumes in storage (without null)
-   */
-  Resume[] getAll() {
-    return Arrays.copyOfRange(storage, 0, size());
-  }
-
-  int size() {
-    int size = 0;
-    while (storage[size] != null) {
-      size++;
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
     }
-    return size;
-  }
+
+    int size() {
+        return this.size;
+    }
 }
