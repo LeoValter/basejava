542146daaceb08c215dabf00feed26f1725574e1
leovalter <leovalter83@gmail.com>
2020-08-19 17:02:54 +0300

    HW01 - implementation of methods in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
 
-    void clear() {
+  Resume[] storage = new Resume[10000];
+
+  void clear() {
+    Arrays.fill(storage, 0, size(), null);
+  }
+
+  void save(Resume r) {
+    if (r == null) {
+      throw new NullPointerException();
     }
 
-    void save(Resume r) {
+    if (size() > storage.length) {
+      throw new IndexOutOfBoundsException();
     }
 
-    Resume get(String uuid) {
-        return null;
-    }
+    storage[size()] = r;
+  }
 
-    void delete(String uuid) {
+  Resume get(String uuid) {
+    int size = size();
+    for (int i = 0; i < size; i++) {
+      if (uuid.equals(storage[i].uuid)) {
+        return storage[i];
+      }
     }
+    return null;
+  }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
+  void delete(String uuid) {
+    int size = size();
+    for (int i = 0; i < size; i++) {
+      if (uuid.equals(storage[i].uuid)) {
+        System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+        storage[size - 1] = null;
+        return;
+      }
     }
+  }
 
-    int size() {
-        return 0;
+  /**
+   * @return array, contains only Resumes in storage (without null)
+   */
+  Resume[] getAll() {
+    return Arrays.copyOfRange(storage, 0, size());
+  }
+
+  int size() {
+    int size = 0;
+    while (storage[size] != null) {
+      size++;
     }
+    return size;
+  }
 }
