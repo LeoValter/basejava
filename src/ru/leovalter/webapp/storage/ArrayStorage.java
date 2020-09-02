0d961289d00e584984092446e2fe097aba4926a0
leovalter <leovalter83@gmail.com>
2020-09-02 17:53:27 +0300

    add AbstractArrayStorage & SortedArrayStorage

--- src/ru/leovalter/webapp/storage/ArrayStorage.java
+++ src/ru/leovalter/webapp/storage/ArrayStorage.java
@@ -7,12 +7,7 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage {
-
-    private static final int STORAGE_LIMIT = 10000;
-
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -63,11 +58,7 @@ public class ArrayStorage implements Storage {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
-    public int size() {
-        return size;
-    }
-
-    private int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
@@ -75,5 +66,4 @@ public class ArrayStorage implements Storage {
         }
         return -1;
     }
-
 }
