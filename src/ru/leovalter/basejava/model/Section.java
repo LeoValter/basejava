13c6fd3ee73d617c8568e4742ad9fe1b39f3b0e5
leovalter <leovalter83@gmail.com>
2020-09-29 18:15:17 +0300

    ObjectStreamStorage

--- src/ru/leovalter/basejava/model/Section.java
+++ src/ru/leovalter/basejava/model/Section.java
@@ -1,4 +1,6 @@
 package ru.leovalter.basejava.model;
 
-abstract public class Section {
+import java.io.Serializable;
+
+abstract public class Section implements Serializable {
 }
--- src/ru/leovalter/basejava/model/TextSection.java
+++ src/ru/leovalter/basejava/model/TextSection.java
@@ -3,6 +3,9 @@ package ru.leovalter.basejava.model;
 import java.util.Objects;
 
 public class TextSection extends Section {
+
+    private static final long serialVersionUID = 1L;
+
     private final String content;
 
     public TextSection(String content) {
