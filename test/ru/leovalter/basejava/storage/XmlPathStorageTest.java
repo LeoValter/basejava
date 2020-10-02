3802bebbbe0e441f0adb4000aa8b157529d6250b
leovalter <leovalter83@gmail.com>
2020-10-02 16:51:56 +0300

    XML

---
+++ test/ru/leovalter/basejava/storage/XmlPathStorageTest.java
@@ -0,0 +1,10 @@
+package ru.leovalter.basejava.storage;
+
+import ru.leovalter.basejava.storage.serializer.XmlStreamSerializer;
+
+public class XmlPathStorageTest extends AbstractStorageTest {
+
+    public XmlPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new XmlStreamSerializer()));
+    }
+}
