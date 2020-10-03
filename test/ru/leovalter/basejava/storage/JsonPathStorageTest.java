cf5c3cd862bc8755a4359e6ddefaa9a3d28325a9
leovalter <leovalter83@gmail.com>
2020-10-03 16:39:51 +0300

    Json

---
+++ test/ru/leovalter/basejava/storage/JsonPathStorageTest.java
@@ -0,0 +1,10 @@
+package ru.leovalter.basejava.storage;
+
+import ru.leovalter.basejava.storage.serializer.JsonStreamSerializer;
+
+public class JsonPathStorageTest extends AbstractStorageTest {
+
+    public JsonPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new JsonStreamSerializer()));
+    }
+}
