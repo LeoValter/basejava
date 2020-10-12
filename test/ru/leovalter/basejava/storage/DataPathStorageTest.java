105ec27d250d2ef43a3991e5e983609531f03b0e
leovalter <leovalter83@gmail.com>
2020-10-12 17:52:59 +0300

    DataStream

---
+++ test/ru/leovalter/basejava/storage/DataPathStorageTest.java
@@ -0,0 +1,10 @@
+package ru.leovalter.basejava.storage;
+
+import ru.leovalter.basejava.storage.serializer.DataStreamSerializer;
+
+public class DataPathStorageTest extends AbstractStorageTest {
+
+    public DataPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new DataStreamSerializer()));
+    }
+}
