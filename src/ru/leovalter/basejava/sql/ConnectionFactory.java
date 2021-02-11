ad7569b58a2fcb34bdd9d8b9bbaa95d18344ccbc
leovalter <leovalter83@gmail.com>
2021-02-11 23:35:03 +0300

    SqlStorage

---
+++ src/ru/leovalter/basejava/sql/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package ru.leovalter.basejava.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionFactory {
+    Connection getConnection() throws SQLException;
+}
